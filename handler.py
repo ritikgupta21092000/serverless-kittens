24ada6f2d5c37b0f272bc185e56fcae250e56441
ritikgupta21092000 <guptaritik349@gmail.com>
2022-10-04 13:15:42 +0530

    Added post and get item

--- handler.py
+++ handler.py
@@ -29,13 +29,13 @@ def hello(event, context):
 def kittens_create(event, context):
     print("Event: ", event)
     print("Context: ", context)
-    dynamodb = boto3.resource("dynamodb")
+    dynamodb = boto3.client("dynamodb")
     try:
         createResults = dynamodb.put_item(
             TableName=os.environ["DYNAMODB_TABLE_NAME"],
             Item={
-                "kittenName": {"S": "Fluffykins"},
-                "kittenAge": {"N": 22}
+                "kittenName": {"S": "Tom"},
+                "kittenAge": {"N": "20"}
             }
         )
     except Exception as e:
@@ -45,10 +45,11 @@ def kittens_create(event, context):
             "message": "Some error occured!"
         }
     print(createResults)
-    return {
+    response = {
         "statusCode": 200,
-        "message": "Successfully Retrieved!"
+        "body": json.dumps("Successfully Added Kitten!")
     }
+    return response
 
 
 def kittens_list(event, context):
@@ -57,10 +58,14 @@ def kittens_list(event, context):
     dynamodb = boto3.client("dynamodb")
     getResults = ""
     try:
-        getResults = dynamodb.get_item(
+        getResults = dynamodb.scan(
             TableName=os.environ["DYNAMODB_TABLE_NAME"],
-            Key={"kittenName": {"S": "Fluffykins"}}
+            ProjectionExpression="kittenName, kittenAge"
         )
+        # getResults = dynamodb.get_item(
+        #     TableName=os.environ["DYNAMODB_TABLE_NAME"],
+        #     Key={"kittenName": {"S": "Fluffykins"}}
+        # )
     except Exception as e:
         print(e)
         return {
@@ -68,10 +73,11 @@ def kittens_list(event, context):
             "message": "Some error occured!"
         }
     print("Get Result", getResults)
-    return {
+    response = {
         "statusCode": 200,
-        "message": "Successfully Retrieved!"
+        "body": json.dumps(getResults["Items"])
     }
+    return response
 
 
 def kittens_update(event, context):
